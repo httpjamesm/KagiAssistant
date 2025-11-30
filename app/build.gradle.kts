b53d0952dc657ff35119d65c5276f8d4aa7feea1
httpjamesm <github@httpjames.space>
2025-11-30 15:10:02 -0500

    version 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "space.httpjames.kagiassistantmaterial"
         minSdk = 36
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
